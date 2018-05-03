106241289a876453d26b9e458b50ef78408d5878
Tin Dvorski <tin.dvorski@student.mev>
2018-05-03 16:29:27 +0200

    Modeli

---
+++ Ishodi/Ishodi/Models/Program_kolegij.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ishodi.Models
+{
+    public class Program_kolegij
+    {
+        public string id_program { get; set; }
+        public string id_kolegij { get; set; }
+        public int semestar { get; set; }
+
+
+    }
+}
